a280976e4600e741ef58dc0f580155d39254d695
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-11-10 12:08:24 +0200

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const morgan = require('morgan')
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
 //app.use(morgan("combined"));
 
 //app.use(morgan(':date[iso] :method :url :http-version :user-agent :status (:response-time ms)'));
 
-
+app.use(morgan((tokens, req, res)=> {
+  //conver the body boject to string first
+  const requestBody = JSON.stringify(req.body)
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    requestBody,
+  ].join(' ')
+}));
 
 let persons = [
   { 
